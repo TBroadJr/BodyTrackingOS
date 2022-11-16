c256b4488465744c1c9d26113291551fa9bdabb0
Tornelius <tkbjr28@icloud.com>
2022-11-16 14:09:29 -0500

    Initial Commit

---
+++ BodyTrackingOS/BodyTrackingOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackingOSApp.swift
+//  BodyTrackingOS
+//
+//  Created by Tornelius Broadwater, Jr on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackingOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
